f343b25cb1a63c056fb4af9cbcb3f8957fd058c9
Dev Modi <devmodi910@gmail.com>
2025-02-08 12:20:18 +0530

    project done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,23 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useRef,useEffect } from 'react';
 import { createPortal } from 'react-dom';
-
-const Modal = forwardRef(function Modal({ children }, ref) {
+function Modal({ open,children,onClose }) {
   const dialog = useRef();
+  useEffect(()=>{
+    if(open){
+      dialog.current.showModal();
+    }
+    else{
+      dialog.current.close()
+    }
+  },[open])
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children: null}
     </dialog>,
     document.getElementById('modal')
   );
-});
+};
 
 export default Modal;
